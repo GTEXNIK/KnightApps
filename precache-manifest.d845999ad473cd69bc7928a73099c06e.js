3365a38ac16682b07ca2369e736610060b5924a5
GTEXNIK <65672058+GTEXNIK@users.noreply.github.com>
2020-08-27 00:27:35 +0300

    Updates

---
+++ precache-manifest.d845999ad473cd69bc7928a73099c06e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868b9cd9896102ad2dfde5c9f1fed7c0",
+    "url": "/KnightApps/index.html"
+  },
+  {
+    "revision": "b2305483ba20662b7609",
+    "url": "/KnightApps/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ca228922345b081d01cc",
+    "url": "/KnightApps/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b2305483ba20662b7609",
+    "url": "/KnightApps/static/js/2.a58e76b7.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/KnightApps/static/js/2.a58e76b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca228922345b081d01cc",
+    "url": "/KnightApps/static/js/main.6d8680e3.chunk.js"
+  },
+  {
+    "revision": "a0badb258ed086195053",
+    "url": "/KnightApps/static/js/runtime-main.05da7e55.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/KnightApps/static/media/persik.4e1ec840.png"
+  }
+]);
